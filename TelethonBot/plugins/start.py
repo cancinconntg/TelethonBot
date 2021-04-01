cebc6d517bca60aac4cf6569689ef77528cd0957
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-01 11:28:10 +0300

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -14,12 +14,3 @@ async def start(event):
 async def ex(event):
     await event.edit("You clicked a button!")
 
-@BotzHub.on(events.NewMessage(incoming=True, pattern="/etiket"))
-users = teleb.get_participants(1001472566181)
-print(users[0].first_name)
-
-for user in users:
-    if user.username is not None:
-        print(user.username)
-        teleb.send_message(1001472566181, "@{}".format(user.username))
-        time.sleep(2)
