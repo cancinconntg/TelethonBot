20ff05d0158d1dfa8782f19a31053f968a7b639e
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-02 01:12:01 +0300

    Update xeneme.py

--- TelethonBot/plugins/xeneme.py
+++ TelethonBot/plugins/xeneme.py
@@ -1,14 +1,17 @@
+from .. import BotzHub
+from telethon import events, Button
+
 import telebot
 
 TOKEN = "1699009884:AAHjE7wUGrSQgLUlIWgzucaV91dVJSDZhVs" #@BotFather
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=["start"])
+@BotzHub.message_handler(commands=["start"])
 def start(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, "Mandame un privado con '/join {0}'".format(chat_id))
 
-@bot.message_handler(commands=["join"])
+@BotzHub.message_handler(commands=["join"])
 def join(message):
     print "group: " + message.text.replace("/join","").replace(" ","")
     user_id = str(message.from_user.id)
@@ -23,7 +26,7 @@ def join(message):
         id_file.write("{0}\n".format(user_id))
     id_file.close()
 
-@bot.message_handler(commands=["all@mentionicindenemebot","all"])
+@BotzHub.message_handler(commands=["all@mentionicindenemebot","all"])
 def all(message):
     ctext = message.text.replace("/all ","")
     id_list = open("id.txt","r").readlines()
@@ -32,8 +35,8 @@ def all(message):
         #bot.send_message(int(user_id), text)
         bot.send_message(int(user_id), "@" + str(message.from_user.username) + " (*" + message.chat.title + "*): " + ctext + "\n", parse_mode = 'Markdown')
         
-@bot.message_handler(commands=["data"])
+@BotzHub.message_handler(commands=["data"])
 def data(message):
     print(message)
 
-bot.polling()
+BotzHub.polling()
