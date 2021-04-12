8b62d300dd674627487dcfd41055e148404ee9cb
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-12 16:24:08 +0300

    Update xeneme.py

--- TelethonBot/plugins/xeneme.py
+++ TelethonBot/plugins/xeneme.py
@@ -5,19 +5,14 @@ from telethon import TelegramClient, sync, events, utils, types, Button
 from telethon.events import StopPropagation
 from telethon.sync import TelegramClient
 
-api_id = 3926716 # int
-api_hash = '2d0c249f0efe0fe1ea2551703a2f774d'
-bot_token = "1725823055:AAGKZJFOdnrtEGlhR9aRA3CZONmiSZ-Ulyg"
-client = TelegramClient('bot_token', api_id, api_hash)
-
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/starrt"))
 def basla(message):
 
-entity=client.get_entity("@Saygisizlar")
-users = client.get_participants(entity)
+entity= BotzHub.get_entity("@Saygisizlar")
+users = BotzHub.get_participants(entity)
 print(len(users[0].first_name))
      
 for user in users:
      if user.username is not None:
           print(user.username)
-          client.send_message(message.chat.id,f"@{user.username}")
+          BotzHub.send_message(-1001472566181,f"@{user.username}")
