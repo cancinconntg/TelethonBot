37d3849e2431b9f7466e66bc400802329689dc74
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-12 16:21:56 +0300

    Update __init__.py

--- TelethonBot/__init__.py
+++ TelethonBot/__init__.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+APP_ID = 3926716 # int
+API_HASH = '2d0c249f0efe0fe1ea2551703a2f774d' 
+BOT_TOKEN = "1725823055:AAGKZJFOdnrtEGlhR9aRA3CZONmiSZ-Ulyg"
 
 BotzHub = TelegramClient('BotzHub', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
