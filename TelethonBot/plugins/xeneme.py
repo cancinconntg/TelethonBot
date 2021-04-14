21895e28734cb42b1a525c102a2ab24a1fb5cbdd
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-14 12:13:59 +0300

    Update xeneme.py

--- TelethonBot/plugins/xeneme.py
+++ TelethonBot/plugins/xeneme.py
@@ -27,7 +27,7 @@ if not client.is_user_authorized():
      client.send_code_request(bot_token)
 
 entity=client.get_entity("chat_id")
-users = client.get_participants(entity)
+users = client.get_participants(chat_id)
 print(len(users[0].first_name)) 
 
 if not user.bot:
