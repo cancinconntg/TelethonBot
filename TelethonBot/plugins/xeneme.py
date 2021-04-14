4127ddd17001b8f8798f2cff9477ac707e452574
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-14 11:33:29 +0300

    Update xeneme.py

--- TelethonBot/plugins/xeneme.py
+++ TelethonBot/plugins/xeneme.py
@@ -5,15 +5,15 @@ from telethon import TelegramClient, sync, events, utils, types, Button
 from telethon.events import StopPropagation
 from telethon.sync import TelegramClient
 
-@BotzHub.on(events.NewMessage(incoming=True, pattern="/starrt"))
-def basla(message):
-    entity=BotzHub.get_entity(chat_id)
-    for user in BotzHub.get_participants(entity)
+@BotzHub.on(events.NewMessage(pattern='basla'))
+async def handler(event):
+    entity=BotzHub.get_entity('chat_id')
+    for user in BotzHub.get_participants('entity')
     if not user.bot:
           if user.username != None:
         	  print(user.username)
         	  Mention.append(user.username)
      
 for etiket in Mention:
-    BotzHub.send_message(chat_id,f"@{etiket}")
+    BotzHub.send_message('chat_id',f"@{etiket}")
     Mention.remove(etiket)4 
