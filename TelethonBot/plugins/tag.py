dd960bd75e7a44f0fd5eccb671016bd64b0230d7
cancinconntg <78117100+cancinconntg@users.noreply.github.com>
2021-04-29 20:42:39 +0300

    Update tag.py

--- TelethonBot/plugins/tag.py
+++ TelethonBot/plugins/tag.py
@@ -12,23 +12,27 @@ from telethon.tl.functions.channels import JoinChannelRequest
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/startt"))
 async def tag(event):
     chat_id = message.chat.id
-    BotzHub.send_message(chat_id, "/join {}".format(chat_id))
+    BotzHub.send_message(chat_id,"/join {}".format(chat_id))
 
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/join"))
 def join(message):
-    print("group: " + message.text.replace("/join","").replace(" ","")) 
-    user_id = str(message.from_user.id)
-    id_file = open("id.txt","a+")
-    id_list = id_file.readlines()
-    estar = False
-    print(user_id, id_list) 
-    for i in id_list:
-        if i.replace("\n","") == user_id:
-            estar = True
-    if not estar:
-        id_file.write("{0}\n".format(user_id))
-    id_file.close()
+    print("hmmmm")
+    user_i = client.iter_participants(chat_id)
+    print(len(users[0].first_name))
+    for user_id in user_i:
+        if user.username is not None:
+            print(user.username)
+            id_file = open("id.txt","a+")
+            id_list = id_file.readlines()
+            estar = False
+            print(user_id, id_list)
+            for i in id_list:
+                if i.replace("\n","") == user_id:
+                    estar = True
+                    if not estar:
+                        id_file.write("{0}\n".format(user_id))
+                        id_file.close()
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/etiket"))
 def all(message):
@@ -37,7 +41,7 @@ def all(message):
     for i in id_list:
         user_id = i.replace("\n","")
         #BotzHub.send_message(int(user_id), text)
-        BotzHub.send_message(int(user_id), "@" +str(message.from_user.username) + " (*" + message.chat.title + "*): " + ctext + "\n" parse_mode = 'Markdown')
+        BotzHub.send_message(int(user_id), "@" +str(message.from_user.username) + " (*" + message.chat.title + "*): " + ctext + "\n""parse_mode = 'Markdown'")
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/data"))
 def data(message):
